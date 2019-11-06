8d52f31072774e57364a5b21026d5c8f290c0fde
nick.zhuyj <nick.zhuyj@cn.ibm.com>
2019-11-06 10:36:36 +0800

    vue router

---
+++ src/router/router.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import VueRouter from 'vue-router'
+
+import Blank from '../views/blank.vue'
+
+Vue.use(VueRouter);
+
+const constantRoutes = [
+    { path: "/", component: Blank }
+];
+
+// 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+    routes: constantRoutes
+});
+
+
+export default router
